e27fae2899047c5bd135c56926b24833ca35b61f
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2020-10-16 21:28:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+document.getElementById('l').style.display = 'none';
+document.getElementById('b').addEventListener('click', function1, false)
+
+
+        function function1() {
+                var x = document.getElementById('l');       
+            
+            if (x.style.display ==='none') {
+                 x.style.display = 'block';
+            } else {
+                  x.style.display = 'none';
+ }
+}
+
+
+
+
+
+
